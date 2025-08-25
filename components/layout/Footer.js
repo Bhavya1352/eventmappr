413f30efa936f6da949e8233d7e9e99ca47588bc
Verma-MK <pncmfp@gmail.com>
2025-08-25 19:41:26 +0530

    Minor Bug Fix

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -63,7 +63,7 @@ const Footer = () => {
                 <div className="social-links">
                   <a href="https://twitter.com/eventmappr" target="_blank" rel="noopener noreferrer" aria-label="X">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1227" width="20" height="20" fill="currentColor">
-                      <path d="M1134 0L703 509l497 718H785l-321-468L126 1227H0l454-542L0 0h427l290 423L949 0h185z"/>
+                      <path d="M1134 0L703 509l497 718H785l-321-468L126 1227H0l454-542L0 0h427l290 423L949 0h185z" />
                     </svg>
                   </a>
                   <a href="https://facebook.com/eventmappr" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
@@ -78,10 +78,10 @@ const Footer = () => {
                     rel="noopener noreferrer"
                     aria-label="Instagram">
                     <svg className="size-6 text-pink-500 hover:text-pink-600 transition-colors"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="1.2em" height="1.2em"
-                        viewBox="0 0 24 24">
-                      <path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/>
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1.2em" height="1.2em"
+                      viewBox="0 0 24 24">
+                      <path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3" />
                     </svg>
                   </a>
                   <a href="https://github.com/Bhavya1352/eventmappr" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
